8992c400bf4d5e6fbf5ec0429c61df933993ad75
tharak1 <saitharakreddyv59@gmail.com>
2024-07-11 21:52:23 +0530

    modified:   src/screens/LandingPage/About.tsx

--- src/screens/LandingPage/About.tsx
+++ src/screens/LandingPage/About.tsx
@@ -21,7 +21,7 @@ const About: React.FC = () => {
           </h3>
         </div>
 
-        <p className="mt-10 px-5 md:px-20 text-xl font-poppins">
+        <p className="mt-10 px-5 md:px-20 text-xl max-sm:text-base font-poppins">
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent mauris. Fusce nec tellus sed augue semper porta. Mauris massa. Vestibulum lacinia arcu eget nulla. <br />
           Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Curabitur sodales ligula in libero. Sed dignissim lacinia nunc. Curabitur tortor. Pellentesque nibh. Aenean quam. In scelerisque sem at dolor. Maecenas mattis. Sed convallis tristique sem. Proin ut ligula vel nunc egestas porttitor. <br />
           Morbi lectus risus, iaculis vel, suscipit quis, luctus non, massa. Fusce ac turpis quis ligula lacinia aliquet. Mauris ipsum. Nulla metus metus, ullamcorper vel, tincidunt sed, euismod in, nibh. Quisque volutpat condimentum velit. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. <br />
