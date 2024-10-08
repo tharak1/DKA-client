42be26916b1423717dd697bc3bc61904c4cf959b
tharak1 <saitharakreddyv59@gmail.com>
2024-10-08 20:14:59 +0530

    modified:   src/components/WriteExamCard.tsx

--- src/components/WriteExamCard.tsx
+++ src/components/WriteExamCard.tsx
@@ -46,7 +46,11 @@ const WriteExamCard:React.FC<WriteExamCardPrpos> = ({qp}) => {
             const encodedUserData = encodeURIComponent(JSON.stringify(curUser));
     
             if (isDateTimeInRange(qp.startDate, qp.startTime, qp.endDate, qp.endTime)) {
-                window.open(`https://dka-exam-portal.vercel.app/write_exam?id=${qp.id}&user=${encodedUserData}`);
+                // window.open(`https://dka-exam-portal.vercel.app/write_exam?id=${qp.id}&user=${encodedUserData}`);
+                // window.location.href = `https://dka-exam-portal.vercel.app/write_exam?id=${qp.id}&user=${encodedUserData}`;
+                window.location.href = `http://localhost:5175/write_exam?id=${qp.id}&user=${encodedUserData}`;
+
+
             } else {
                 setError("Time");
                 setNotification({
